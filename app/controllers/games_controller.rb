573e5025e8cf0eec4943f3216bc68fb3938b1c9e
georgiana chiriac <georgianachiriacweb@gmail.com>
2021-02-15 18:25:41 -0500

    score and new pages

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,25 @@
-#
 class GamesController < ApplicationController
   def new
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
-    # @grid = generate_grid(10)
-    # puts @grid.join(" ")
+    @letters.shuffle!
   end
 
   def score
-    raise
+    @letters = params[:letters].split
+    @word = (params[:word] || '').upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
